465f313e9bab3cb0bd313b0c18bf45a536a924bb
hersh <hershkumar13@gmail.com>
2019-06-21 11:05:05 -0400

    Move introductory stuff to Introductory

--- Z4/Z4GaugeTheory.py
+++ Z4/Z4GaugeTheory.py
@@ -4,7 +4,12 @@ from math import pi
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import Aer, execute
 from qiskit.providers.aer import StatevectorSimulator
+num_qubits = 4
+q = QuantumRegister(num_qubits)
+c = ClassicalRegister(num_qubits)
 
+
+circuit = QuantumCircuit(q,c)
 simulator = Aer.get_backend('statevector_simulator')
 result = execute(circuit,simulator).result()
 statevector = result.get_statevector(circuit)
